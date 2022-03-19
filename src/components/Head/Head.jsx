ceea53594edfd7e37eb0d9eae6a1729e9e78b293
Sarfaraj Shah <sarfarajshah009@gmail.com>
2022-03-19 19:04:02 +0530

    added particles

---
+++ src/components/Head/Head.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './head.css';
+
+const Head = () => {
+  return (
+    <div className='head-container'>
+        <p className="head-title">Name</p>
+        <ul>
+            <li>name1</li>
+            <li>name2</li>
+            <li>name3</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Head;
